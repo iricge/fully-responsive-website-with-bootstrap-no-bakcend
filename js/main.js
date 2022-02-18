cd963d432f0dced71ab5f4e6090e0c503f7be464
Iric Gorgi <iricgorgi@gmail.com>
2022-02-18 20:18:40 +0330

    the final version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var image = ['images/top1.jpg','images/top2.jpg','images/top3.jpg'];
+  var h = document.getElementById('x');
+  function giril(){
+    h.src=image[2];
+  }
+  function sahel(){
+    h.src=image[1];
+  }
+  function abitch(){
+    h.src=image[0];
+  }
+  function ayere1() {
+  }
